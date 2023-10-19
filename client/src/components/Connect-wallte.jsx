10e1673024405174b5de55a241725e279c583a46
Nguyễn Doãn Tùng <95358274+tung0101@users.noreply.github.com>
2023-10-19 18:59:07 +0700

    update 19/10/2023 7h

--- client/src/components/Connect-wallte.jsx
+++ client/src/components/Connect-wallte.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+
 const Connect = () => {
   return (
-    <div className="w-[560px] h-[698px] bg-slate-200 ">
-    </div>
-  )
-}
+    <div className="w-[560px] h-[698px] ">
+      <div className="flex justify-center">
+        <div className="w-[379px] h-[64px] bg-[#2F3338] rounded-full flex gap-10 justify-center items-center">
+          <div className="w-[164px] h-[56px] bg-[#3F444E] rounded-full flex justify-center items-center">
+            <p className="text-[#FFFFFF]">Swap</p>
+          </div>
+          <div className="w-[164px] h-[56px] rounded-full flex justify-center items-center">
+            <p className="text-[#FFFFFF]">Donate</p>
+          </div>
+        </div>
+      </div>
 
-export default Connect
+      <div>
+        <div className="w-[560px] h-[610px] bg-[#2F3338] rounded-lg mt-3 flex justify-center">
+          <div className="flex text-white gap-20 w-[480px] h-[518px] bg-black">
+            <h2>Swap</h2>
+            <div className="w-[32px] h-[32px] bg-[#3F444E] rounded-md">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Connect;
--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import Connect from "../components/Connect-wallte";
+
 const Content = () => {
   return (
-    <div className=" ">
+    <div className="flex items-center justify-center h-screen mt-20">
+      <Connect />
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
