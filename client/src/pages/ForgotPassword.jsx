bde2005260333d7d0efc81388009edd72f7497fe
Nguyễn Doãn Tùng <95358274+tung0101@users.noreply.github.com>
2023-11-09 13:49:38 +0700

    push

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { PiWarningCircle } from "react-icons/pi";
 const ForgotPassword = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col items-center justify-center gap-[40px]">
-        <h1 className="width-[261px] h-[39px] font-[600] text-[32px] text-center mb-[40px]">
+        <h1 className="width-[261px] h-[39px] font-[600] text-[32px] text-center mb-[40px] text-white">
           Forgot Password
         </h1>
         <div className="w-[499px] h-[462px] rounded-[12px] py-[40px] px-[30px] gap-[10px] bg-[#2F3338]">
           <div className=" w-[439px] h-[382px] gap-[30px]">
             {/* input */}
             <div className="h-[75px] gap-[8px]">
-              <p className="w-[42px] h-[19px] font-[500] text-[16px] tracking-[0.5%]">
+              <p className="w-[42px] h-[19px] font-[500] text-[16px] tracking-[0.5%] text-white">
                 Email
               </p>
               <input
-                className="w-full h-[48px] rounded-[8px] py-[12px] px-[16px] gap-[12px] mt-[10px]"
+                className="w-full h-[48px] rounded-[8px] py-[12px] px-[16px] gap-[12px] mt-[10px] bg-[#3F444E]
+                "
                 placeholder="Email"
               />
             </div>
 
             {/* warning */}
-            <div className="flex justify-center  w-[400px] h-[72px] gap-[8px] my-[50px]">
-              <FontAwesomeIcon
-                icon={faCircleInfo}
-                className="w-[24px] h-[24px]"
-              />
+            <div className="mx-auto flex justify-center  w-[400px] h-[72px] gap-[8px] my-[50px]">
+              <PiWarningCircle className="w-[24px] h-[24px] text-[#E1AC7B]" />
               <p className="w-[368px] h-[72px] font-[400] text-[18px] text-[#E1AC7B] text-justify">
                 We can't seen to find the right email address for you, resend
                 the email that you have registered
@@ -34,14 +32,14 @@ const ForgotPassword = () => {
             </div>
 
             {/* btn */}
-            <div className="w-[400px] h-[50px] rounded-[8px] py-[14px] px-[16px] gap-[10px] bg-[#6F49FD]">
-              <p className="w-[123px] h-[20px] font-[500] text-[16px] leading-[20px] capitalize m-auto ">
+            <div className="mx-auto w-[400px] h-[50px] rounded-[8px] py-[14px] px-[16px] gap-[10px] bg-[#6F49FD]">
+              <p className="w-[123px] h-[20px] font-[500] text-[16px] leading-[20px] capitalize m-auto text-white ">
                 reset password
               </p>
             </div>
 
-            <div className="w-[400px] h-[50px] rounded-[8px] py-[14px] px-[16px] gap-[10px] border-[#E8E8E8] border-[1px] border-solid mt-[20px] ">
-              <p className="w-[123px] h-[20px] font-[500] text-[16px] leading-[20px] capitalize m-auto ">
+            <div className="mx-auto w-[400px] h-[50px] rounded-[8px] py-[14px] px-[16px] gap-[10px] border-[#E8E8E8] border-[1px] border-solid mt-[20px] ">
+              <p className="w-[123px] h-[20px] font-[500] text-[16px] leading-[20px] capitalize m-auto text-white">
                 Back to Sign in
               </p>
             </div>
